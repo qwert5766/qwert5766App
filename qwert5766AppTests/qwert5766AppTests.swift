c64f660faaaaa829809833cd61ee3ba577d859e6
qwert5766 <qwert5766@mail.cjcu.edu.tw>
2019-05-27 09:58:56 +0800

    Initial Commit

---
+++ qwert5766AppTests/qwert5766AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  qwert5766AppTests.swift
+//  qwert5766AppTests
+//
+//  Created by L20102 on 2019/5/27.
+//  Copyright © 2019 CJCU. All rights reserved.
+//
+
+import XCTest
+@testable import qwert5766App
+
+class qwert5766AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
